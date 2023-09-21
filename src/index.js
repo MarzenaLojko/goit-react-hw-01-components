f462850b5c207160e951ecf95a155c8f4624ad82
Marzena Łojko <marzena.sidorenko@gmail.com>
2023-09-21 17:50:54 +0200

    1 task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Profile } from 'components/Profile';
 import './index.css';
+import user from './user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   </React.StrictMode>
 );
